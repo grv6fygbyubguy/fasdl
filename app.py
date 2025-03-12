bb5a1888a8df958ed91969f3b981a64e732a4b30
grv6fygbyubguy <bimuaryal55@gmail.com>
2025-03-12 07:52:36 +0545

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, send_from_directory
 from chatbot import chatbot_response
 
 app = Flask(__name__)
@@ -12,6 +12,11 @@ def chat():
     user_input = request.json.get("message")
     bot_response = chatbot_response(user_input)
     return jsonify({"response": bot_response})
+
+# Google Search Console Verification Route
+@app.route("/google874f8f9f03996c33.html")
+def google_verification():
+    return send_from_directory("static", "google874f8f9f03996c33.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
